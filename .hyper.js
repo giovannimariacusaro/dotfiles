326dafd1ca9f93c3b2bba1602941fcfa2f29cca5
giovanni <giovanni@MacBook.local>
2020-11-24 10:51:32 +0100

    Remove unnecessary comments

--- .hyper.js
+++ .hyper.js
@@ -18,10 +18,8 @@ module.exports = {
 
 		cursorColor: '#cbc3b7',
 
-		// terminal text color under BLOCK cursor
 		cursorAccentColor: '#cbc3b7',
 
-		// `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
 		cursorShape: 'BEAM',
 
 		cursorBlink: true,
@@ -34,27 +32,15 @@ module.exports = {
 
 		borderColor: '#060519',
 
-		// custom CSS to embed in the main window
 		css: '',
 
-		// custom CSS to embed in the terminal window
 		termCSS: '',
 
-		// if you're using a Linux setup which show native menus, set to false
-		// default: `true` on Linux, `true` on Windows, ignored on macOS
 		showHamburgerMenu: '',
-
-		// set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
-		// additionally, set to `'left'` if you want them on the left, like in Ubuntu
-		// default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
 		showWindowControls: '',
 
-		// custom padding (CSS format, i.e.: `top right bottom left`)
 		padding: '0 7px 6px 7px',
 
-		// the full list. if you're going to provide the full color palette,
-		// including the 6 x 6 color cubes and the grayscale map, just provide
-		// an array here instead of a color map object
 		colors: {
 			black: '#201f1f',
 			red: '#ea2617',
@@ -74,50 +60,22 @@ module.exports = {
 			lightWhite: '#d6d6dd',
 		},
 
-		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-		// if left empty, your system's login shell will be used by default
-		//
-		// Windows
-		// - Make sure to use a full path if the binary name doesn't work
-		// - Remove `--login` in shellArgs
-		//
-		// Bash on Windows
-		// - Example: `C:\\Windows\\System32\\bash.exe`
-		//
-		// PowerShell on Windows
-		// - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
 		shell: '',
 
-		// for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
-		// by default `['--login']` will be used
 		shellArgs: ['--login'],
 
-		// for environment variables
 		env: {},
 
-		// set to `false` for no bell
 		bell: 'false',
 
-		// if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
 		copyOnSelect: false,
 
-		// if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
 		defaultSSHApp: true,
 
-		// if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
-		// selection is present (`true` by default on Windows and disables the context menu feature)
 		quickEdit: false,
 
-		// choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
-		// or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
-		// (inside tmux or vim with mouse mode enabled for example).
 		macOptionSelectionMode: 'vertical',
 
-		// URL to custom bell
-		// bellSoundURL: 'http://example.com/bell.mp3',
-
-		// Whether to use the WebGL renderer. Set it to false to use canvas-based
-		// rendering (slower, but supports transparent backgrounds)
 		webGLRenderer: false,
 
 		hypercwd: {
@@ -128,7 +86,6 @@ module.exports = {
 			border: true,
 			activityColor: '#d2585a',
 		},
-		// for advanced config flags please refer to https://hyper.is/#cfg
 	},
 
 	plugins: [
@@ -141,7 +98,5 @@ module.exports = {
 	localPlugins: [],
 
 	keymaps: {
-		// Example
-		// 'window:devtools': 'cmd+alt+o',
-	},
+		},
 };
